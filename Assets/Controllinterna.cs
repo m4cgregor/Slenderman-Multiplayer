d5670d7babc6c54aebbac4e3379163e99a9811a7
Gregorio D'Angelo <gregoriodangelo@gmail.com>
2021-01-29 20:49:33 -0300

    Control Linterna
    Agregado Script para encender la linterna con la F

---
+++ Assets/Controllinterna.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllinterna : MonoBehaviour
+{
+
+    Light linterna;
+    bool encendida = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        linterna = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+
+        {
+            encendida = !encendida;
+            linterna.enabled = encendida;
+        }
+
+    }
+}
