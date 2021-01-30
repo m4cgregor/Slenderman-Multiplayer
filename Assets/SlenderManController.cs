7387d62886a7258db697568a482e708167f063c4
Gregorio D'Angelo <gregoriodangelo@gmail.com>
2021-01-29 21:15:06 -0300

    Escena Navigation
    NavMesh
    Script Slenderman controller para persseguir a los players.

---
+++ Assets/SlenderManController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlenderManController : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+    public Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        InvokeRepeating(nameof(FindTarget), 0f, 1f);
+       
+;    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void FindTarget() {
+
+        navMeshAgent.destination = target.position;
+    }
+}
